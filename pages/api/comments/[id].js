0363c81d251111e58bd981d68af4533ef82e9799
banchi <icaro_19@hotmail.com>
2021-12-15 15:48:55 +0100

    resolved api/comments/:id

---
+++ pages/api/comments/[id].js
@@ -0,0 +1,20 @@
+import connectDB from "../../../services/mongoose.services";
+import Comment from '../../../models/Comment.model'
+
+const handler = async (req, res) => {
+    const { id } = req.query
+    if (req.method === "DELETE") {
+        const comment = await Comment.findById(id)
+        if (!comment) {
+            res.status(500).json("Comment not found")
+        } else {
+            const deleteComment = await Comment.deleteOne({ id })
+            res.status(422).json('Remove comment', deleteComment)
+        }
+    } else {
+
+        res.status(422).send('req_method_not_supported')
+    }
+}
+
+export default connectDB(handler) 
